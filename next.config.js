d8d15a8fc76d9903943d1dffb045f68e74b345f1
Bhavya031 <2021003493.adit@cvmu.edu.in>
2024-02-20 21:53:32 +0530

    confiig

--- next.config.js
+++ next.config.js
@@ -2,19 +2,20 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-       images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'i.imgur.com',
-        },
-      ],
-    },
-    env: {
-      GITHUB_APP_CLIENT_ID: '3d419ab419c7f047ff99',
-      GITHUB_APP_CLIENT_SECRET: '506da3ba7af5bb9d77f9db5786349a5cf8d3691d',
-      NEXTAUTH_SECRET: 'ldiFvWT9pk9xDSZkPHmw7WHSXPQMh6Vp5tOR/+TusrE=',
-    },
-  }
-   
-  module.exports = nextConfig
+  output: "export",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+      },
+    ],
+  },
+  env: {
+    GITHUB_APP_CLIENT_ID: "3d419ab419c7f047ff99",
+    GITHUB_APP_CLIENT_SECRET: "506da3ba7af5bb9d77f9db5786349a5cf8d3691d",
+    NEXTAUTH_SECRET: "ldiFvWT9pk9xDSZkPHmw7WHSXPQMh6Vp5tOR/+TusrE=",
+  },
+};
+
+module.exports = nextConfig;
