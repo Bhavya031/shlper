66439b47b64331fc313870c569cba4052f0cf53f
Bhavya031 <2021003493.adit@cvmu.edu.in>
2024-02-27 09:16:18 +0530

    remove env

--- next.config.js
+++ next.config.js
@@ -10,11 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    GITHUB_APP_CLIENT_ID: "3d419ab419c7f047ff99",
-    GITHUB_APP_CLIENT_SECRET: "506da3ba7af5bb9d77f9db5786349a5cf8d3691d",
-    NEXTAUTH_SECRET: "ldiFvWT9pk9xDSZkPHmw7WHSXPQMh6Vp5tOR/+TusrE=",
-  },
 };
 
 module.exports = nextConfig;
